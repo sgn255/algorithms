0e0809ae20f372d01fa2d0f23553d366ffd7a27d
scott <scott-green89@hotmail.com>
2019-08-05 16:47:15 +0200

    two_sum.py

---
+++ python/two_sum.py
@@ -0,0 +1,8 @@
+# Given an array of integers:
+#   return the indices of the two numbers that add up to the given target
+
+def two_sum(nums, target):
+        for i in range(len(nums)):
+            temp_target=target-nums[i]
+            if (temp_target in nums) and (nums.index(temp_target) != i):
+                return [i, nums.index(temp_target)]
